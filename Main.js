d2987e7dcdc6bf2f6c99793f9b3274f8371ed934
Christian Parker <71667365+christianparker512@users.noreply.github.com>
2021-04-29 18:23:59 -0500

    Main.js

---
+++ Main.js
@@ -0,0 +1,29 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+//my components
+import Header from "./components/Header"
+import HomeGuest from "./components/HomeGuest"
+import Footer from "./components/Footer"
+import About from "./components/About"
+import Terms from "./components/Terms"
+
+function Main () {
+  return (
+    <>
+      <Header />
+      <About />
+      <HomeGuest />
+      <Terms />
+      
+      <Footer />
+
+    </>
+  )
+}
+
+ReactDOM.render(<Main />, document.querySelector("#app"))
+
+if (module.hot) {
+  module.hot.accept()
+}
