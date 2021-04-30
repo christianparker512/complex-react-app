a429e6882d5e014f714966d5507856922d3ccbde
Christian Parker <71667365+christianparker512@users.noreply.github.com>
2021-04-30 10:31:15 -0500

    HomeGuest.js
    Visual Studio Code

--- HomeGuest.js
+++ HomeGuest.js
@@ -1,28 +1,41 @@
 import React from "react"
-function Header() {
-  <header className="header-bar bg-primary mb-3">
-    <div className="container d-flex flex-column flex-md-row align-items-center p-3">
-      <h4 className="my-0 mr-md-auto font-weight-normal">
-        <a href="/" className="text-white">
-          {" "}
-          ComplexApp{" "}
-        </a>
-      </h4>
-      <form className="mb-0 pt-2 pt-md-0">
-        <div className="row align-items-center">
-          <div className="col-md mr-0 pr-md-0 mb-3 mb-md-0">
-            <input name="username" className="form-control form-control-sm input-dark" type="text" placeholder="Username" autoComplete="off" />
-          </div>
-          <div className="col-md mr-0 pr-md-0 mb-3 mb-md-0">
-            <input name="password" className="form-control form-control-sm input-dark" type="password" placeholder="Password" />
-          </div>
-          <div className="col-md-auto">
-            <button className="btn btn-success btn-sm">Sign In</button>
-          </div>
+
+function HomeGuest() {
+  return (
+    <div className="container py-md-5">
+      <div className="row align-items-center">
+        <div className="col-lg-7 py-3 py-md-5">
+          <h1 className="display-3">Remember Writing?</h1>
+          <p className="lead text-muted">Are you sick of short tweets and impersonal &ldquo;shared&rdquo; posts that are reminiscent of the late 90&rsquo;s email forwards? We believe getting back to actually writing is the key to enjoying the internet again.</p>
         </div>
-      </form>
+        <div className="col-lg-5 pl-lg-5 pb-3 py-lg-5">
+          <form>
+            <div className="form-group">
+              <label htmlFor="username-register" className="text-muted mb-1">
+                <small>Username</small>
+              </label>
+              <input id="username-register" name="username" className="form-control" type="text" placeholder="Pick a username" autoComplete="off" />
+            </div>
+            <div className="form-group">
+              <label for="email-register" className="text-muted mb-1">
+                <small>Email</small>
+              </label>
+              <input id="email-register" name="email" className="form-control" type="text" placeholder="you@example.com" autocomplete="off" />
+            </div>
+            <div className="form-group">
+              <label htmlFor="password-register" className="text-muted mb-1">
+                <small>Password</small>
+              </label>
+              <input id="password-register" name="password" className="form-control" type="password" placeholder="Create a password" />
+            </div>
+            <button type="submit" className="py-3 mt-4 btn btn-lg btn-success btn-block">
+              Sign up for ComplexApp
+            </button>
+          </form>
+        </div>
+      </div>
     </div>
-  </header>
+  )
 }
 
-export default Header
+export default HomeGuest
