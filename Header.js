621017c6fc86512f1a402d9150a188d5296beb40
Christian Parker <71667365+christianparker512@users.noreply.github.com>
2021-05-02 16:23:16 -0500

    Header.js
    Included HeaderLoggedIn.js functionality. import and then set the log in on line 18

--- Header.js
+++ Header.js
@@ -1,9 +1,13 @@
-import React from "react"
+import React,{useState} from "react"
 import {Link} from 'react-router-dom'
 import HeaderLoggedOut from './HeaderLoggedOut'
+import HeaderLoggedIn from './HeaderLoggedIn'
 
 function Header() {
-  <header className="header-bar bg-primary mb-3">
+  const [loogedIn, setLoggedIn]=useState()
+
+return (
+<header className="header-bar bg-primary mb-3">
     <div className="container d-flex flex-column flex-md-row align-items-center p-3">
       <h4 className="my-0 mr-md-auto font-weight-normal">
         <Link to="/" className="text-white">
@@ -11,10 +15,12 @@ function Header() {
           ComplexApp{" "}
         </Link>
       </h4>
+      {loggedIn ? <HeaderLoggedIn setLoggedIn={setLoggedIn} /> : <HeaderLoggedOut setLoggedIn={setLoggedIn} />}
       <HeaderLoggedOut />
       
     </div>
   </header>
+  )
 }
 
 export default Header
