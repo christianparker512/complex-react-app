9735d40d0f200665f5b4d109a9edde81a9806505
Christian Parker <71667365+christianparker512@users.noreply.github.com>
2021-05-03 14:12:21 -0500

    CreatePost.js
    Changes to the createpost onsubmit and on change. Saving posts to mongoDB

--- CreatePost.js
+++ CreatePost.js
@@ -1,22 +1,35 @@
-import React, { useEffect } from "react"
+import React, { useEffect, useState } from "react"
 import Page from './Page'
+import Axios from 'axios'
+import e from "express"
 
 function CreatePost() {
+  const [title, setTitle] = useState()
+  const [body, setBody] = useState()
+  async function handleSubmit(e){
+    e.preventDefault()
+    try {
+      await Axios.post('/create-post', {title, body, token: localStorage.getItem("complexappItem")})
+      console.log("New post was created.")
+    }catch (e){
+      console.log("There was a problem")
+    }
+  }
   return (
-    <Page>
-    <form title="Create New Post">
+    <Page title="Create New Post">
+    <form  onSubmit={handleSubmit}>
         <div className="form-group">
-          <label for="post-title" className="text-muted mb-1">
+          <label htmlFor="post-title" className="text-muted mb-1">
             <small>Title</small>
           </label>
-          <input autofocus name="title" id="post-title" class="form-control form-control-lg form-control-title" type="text" placeholder="" autocomplete="off" />
+          <input onChange = {e => setTitle(e.target.value)}autoFocus name="title" id="post-title" class="form-control form-control-lg form-control-title" type="text" placeholder="" autoComplete="off" />
         </div>
 
         <div class="form-group">
-          <label for="post-body" class="text-muted mb-1 d-block">
+          <label htmlFor="post-body" class="text-muted mb-1 d-block">
             <small>Body Content</small>
           </label>
-          <textarea name="body" id="post-body" class="body-content tall-textarea form-control" type="text"></textarea>
+          <textarea onChange = {e => setBody(e.target.value)} name="body" id="post-body" class="body-content tall-textarea form-control" type="text"></textarea>
         </div>
 
         <button class="btn btn-primary">Save New Post</button>
