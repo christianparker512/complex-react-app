6288ea621dd95d22f1a241481042a1f720c7db27
Christian Parker <71667365+christianparker512@users.noreply.github.com>
2021-05-04 12:34:29 -0500

    CreatePost.js
    With Router

--- CreatePost.js
+++ CreatePost.js
@@ -1,15 +1,18 @@
 import React, { useEffect, useState } from "react"
 import Page from './Page'
 import Axios from 'axios'
-import e from "express"
+import {withRouter} from 'react-router-dom'
 
-function CreatePost() {
+
+function CreatePost(props) {
   const [title, setTitle] = useState()
   const [body, setBody] = useState()
   async function handleSubmit(e){
     e.preventDefault()
     try {
-      await Axios.post('/create-post', {title, body, token: localStorage.getItem("complexappItem")})
+      const response = await Axios.post('/create-post', {title, body, token: localStorage.getItem("complexappItem")})
+      //Redirect to new post url
+      props.history.push('/post/${response.data}')
       console.log("New post was created.")
     }catch (e){
       console.log("There was a problem")
@@ -38,4 +41,4 @@ function CreatePost() {
   )
 }
 
-export default CreatePost
+export default withRouter(CreatePost)
