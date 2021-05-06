ddc1b9b21b24c92fd1222b75bb6890fd05f9829e
Christian Parker <71667365+christianparker512@users.noreply.github.com>
2021-05-06 14:05:21 -0500

    Update CreatePost.js

--- CreatePost.js
+++ CreatePost.js
@@ -2,20 +2,20 @@ import React, { useEffect, useState, useContext} from "react"
 import Page from './Page'
 import Axios from 'axios'
 import {withRouter} from 'react-router-dom'
-import ExampleContext from "../ExampleContext"
+import DispatchContext from "../DispatchContext"
 
 
 function CreatePost(props) {
   const [title, setTitle] = useState()
   const [body, setBody] = useState()
-  const {addFlashMessage} = useContext(ExampleContext)
+  const appDispatch = useContext(DispatchContext)
 
   async function handleSubmit(e){
     e.preventDefault()
     try {
       const response = await Axios.post('/create-post', {title, body, token: localStorage.getItem("complexappItem")})
       //Redirect to new post url
-      props.addFlashMessage("Congrats, you've successfully created a post!")
+      appDispatch({type: "flashMaessage", value: "Congrats you created a new post."})
       props.history.push('/post/${response.data}')
       console.log("New post was created.")
     }catch (e){
