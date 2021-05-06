00deda90b69d84d60cfc019a40eeff15b962ab3f
Christian Parker <71667365+christianparker512@users.noreply.github.com>
2021-05-06 14:03:23 -0500

    Update Header.js

--- Header.js
+++ Header.js
@@ -2,9 +2,10 @@ import React,{useState} from "react"
 import {Link} from 'react-router-dom'
 import HeaderLoggedOut from './HeaderLoggedOut'
 import HeaderLoggedIn from './HeaderLoggedIn'
+import StateContext from '../StateContext'
 
 function Header(props) {
-  
+  const appState = useContext(StateContext)
 
 return (
 <header className="header-bar bg-primary mb-3">
@@ -15,7 +16,7 @@ return (
           ComplexApp{" "}
         </Link>
       </h4>
-      {props.loggedIn ? <HeaderLoggedIn  /> : <HeaderLoggedOut  />}
+      {appState.loggedIn ? <HeaderLoggedIn  /> : <HeaderLoggedOut  />}
       <HeaderLoggedOut />
       
     </div>
