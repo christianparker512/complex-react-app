1edcd66dd36da9eb1cf6cb19705337c97da353a0
Christian Parker <71667365+christianparker512@users.noreply.github.com>
2021-05-08 15:57:41 -0500

    Create LoadingDotsIcon.js

---
+++ LoadingDotsIcon.js
@@ -0,0 +1,14 @@
+import React, { useEffect } from "react"
+
+function LoadingDotsIcon
+() {
+  return (
+    <div className="dots-loading">
+      <div>
+        
+      </div>
+    </div>
+  )
+}
+
+export default LoadingDotsIcon
