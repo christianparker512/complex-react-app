4d1b0aa839d3d97b6b678226d17d76e5a1f0b8b9
Christian Parker <71667365+christianparker512@users.noreply.github.com>
2021-05-08 14:49:56 -0500

    Update ProfilePosts.js

--- ProfilePosts.js
+++ ProfilePosts.js
@@ -24,8 +24,8 @@ function ProfilePosts() {
     <div class="list-group">
     {posts.map(post =>{
       return (
-        <a href="#" class="list-group-item list-group-item-action">
-      <img class="avatar-tiny" src="https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128" /> <strong>Example Post #1</strong>
+        <a key ={post._id} href="#" class="list-group-item list-group-item-action">
+      <img class="avatar-tiny" src={post.author.avatar} /> <strong>{post.title}</strong>
       <span class="text-muted small">on 2/10/2020 </span>
     </a>
       )
