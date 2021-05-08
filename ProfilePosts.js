0e4a978ccfbbb99bd84f96734f25bd005b2e5ef3
Christian Parker <71667365+christianparker512@users.noreply.github.com>
2021-05-08 14:57:40 -0500

    Update ProfilePosts.js

--- ProfilePosts.js
+++ ProfilePosts.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState} from "react"
 import Axios from "axios"
-import {useParam} from 'react=router-dom'
+import {useParam, Link} from 'react=router-dom'
+
 function ProfilePosts() {
   const[isLoading, setIsLoading] = useState(true)
   const [posts, setPosts]=useState([])
@@ -23,11 +24,13 @@ function ProfilePosts() {
   return (
     <div class="list-group">
     {posts.map(post =>{
+      const date = new Date(post.createdDate)
+      const dateFormatted = `${date.getMonth() +1}/${date.getDate()}/${date.getFullYear}`
       return (
-        <a key ={post._id} href="#" class="list-group-item list-group-item-action">
-      <img class="avatar-tiny" src={post.author.avatar} /> <strong>{post.title}</strong>
-      <span class="text-muted small">on 2/10/2020 </span>
-    </a>
+        <Link key ={post._id} to={`/post/${post._id}`} class="list-group-item list-group-item-action">
+      <img class="avatar-tiny" src={post.author.avatar} /> <strong>{post.title}</strong>{" "}
+      <span class="text-muted small">on {dateFormatted} </span>
+    </Link>
       )
     })}
   </div>
