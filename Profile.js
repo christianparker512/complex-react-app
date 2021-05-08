1ad23a0ecee5684f8b045db7ee91125171a9e010
Christian Parker <71667365+christianparker512@users.noreply.github.com>
2021-05-07 22:00:38 -0500

    profile.js update

--- Profile.js
+++ Profile.js
@@ -7,11 +7,18 @@ import StateContext from `../StateContext`
 function Profile() {
   const {username} = useParams()
   const appState = useContext(StateContext)
+  const [profileData, setProfileData] = useState ({
+    profileUsername:"...",
+    profileAvatar:'https://gravatar.com/avatar/placeholder?128',
+    isFollowing: false,
+    counts: {postCount: "", followerCount: "", followingCount: ""}
+  })
 
   useEffect(() => {
     async function fetchData(){
       try {
         const response = await Axios.post(`/profile/${username}`,{token: appState.user.token})
+        setProfileData(response.data)
       }catch (e){
         console.log("there was a problem")
       }
@@ -20,19 +27,19 @@ function Profile() {
   return (
    <Page titl="Profile Screen">
      <h2>
-        <img class="avatar-small" src="https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128" /> brad
+        <img class="avatar-small" src="{profileData.profileAvatar}"/> {profileData.profile.username}
         <button class="btn btn-primary btn-sm ml-2">Follow <i class="fas fa-user-plus"></i></button>
       </h2>
 
       <div class="profile-nav nav nav-tabs pt-2 mb-4">
         <a href="#" class="active nav-item nav-link">
-          Posts: 3
+          Posts: {profileData.counts.postCount}
         </a>
         <a href="#" class="nav-item nav-link">
-          Followers: 101
+          Followers: {profileData.followers.postCount}
         </a>
         <a href="#" class="nav-item nav-link">
-          Following: 40
+          Following: {profileData.following.postCount}
         </a>
       </div>
 
