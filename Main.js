e28f4cb2fc1ab5b3d492875791e9671ecd35e73f
Christian Parker <71667365+christianparker512@users.noreply.github.com>
2021-05-07 21:20:06 -0500

    update main.js
    added the proflile import

--- Main.js
+++ Main.js
@@ -18,7 +18,7 @@ import Terms from "./components/Terms"
 import CreatePost from "./components/CreatePost"
 import ViewSinglePost from "./components/ViewSinglePost"
 import FlashMessages from './components/FlashMessages'
-
+import Profile from './compenents/Profile'
 function Main () {
   const initialState = {
     loggedIn: Boolean(localStorage.getItem("complexappToken")), 
@@ -67,6 +67,9 @@ return (
       <FlashMessages messages={state.flashMessages}/>
       <Header />
         <Switch>
+          <Route path="/profile/:username">
+            <Profile />
+          </Route>
           <Route path="/" exact>
             {state.loggedIn ? <Home /> : <HomeGuest />}
             </Route>
