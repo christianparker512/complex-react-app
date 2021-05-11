bbbacea5dce4058d2680fc727136ebcc7bd235b1
Christian Parker <71667365+christianparker512@users.noreply.github.com>
2021-05-11 13:42:02 -0500

    updated editpost.js

--- EditPost.js
+++ EditPost.js
@@ -1,80 +1,130 @@
-import React, { useEffect, useState } from "react"
-import  {useImmerReducer} from 'use-immer'
-import Page from './Page'
-import {useParams, Link} from 'react-router-dom'
-import Axios from 'axios'
-import LoadingDotsIcon from './LoadingDotsIcon'
-import ReactMarkDown from 'react-markdown'
-import ReactTooltip from 'react-tooltip'
+import React, { useEffect, useState, useContext } from "react"
+import { useImmerReducer } from "use-immer"
+import Page from "./Page"
+import { useParams, Link } from "react-router-dom"
+import Axios from "axios"
+import LoadingDotsIcon from "./LoadingDotsIcon"
+import StateContext from "../StateContext"
+import DispatchContext from "../DispatchContext"
 
 function ViewSinglePost() {
-  const originalState ={
+  const appState = useContext(StateContext)
+  const appDispatch = useContext(DispatchContext)
+
+  const originalState = {
     title: {
       value: "",
       hasErrors: false,
       message: ""
-    }, 
+    },
     body: {
-      alue: "",
+      value: "",
       hasErrors: false,
       message: ""
     },
     isFetching: true,
     isSaving: false,
-    id: useParams().id, 
+    id: useParams().id,
     sendCount: 0
   }
-  function ourReducer(draft,action) {
-    switch(action.type){
-      case"fetchComplete":
-        draft.title.value = action.value.title 
+
+  function ourReducer(draft, action) {
+    switch (action.type) {
+      case "fetchComplete":
+        draft.title.value = action.value.title
         draft.body.value = action.value.body
         draft.isFetching = false
-        return 
+        return
+      case "titleChange":
+        draft.title.value = action.value
+        return
+      case "bodyChange":
+        draft.body.value = action.value
+        return
+      case "submitRequest":
+        draft.sendCount++
+        return
+      case "saveRequestStarted":
+        draft.isSaving = true
+        return
+      case "saveRequestFinished":
+        draft.isSaving = false
+        return
     }
   }
+
   const [state, dispatch] = useImmerReducer(ourReducer, originalState)
 
-useEffect(() => {
-  const ourRequest = Axios.CancelToken.source()
-  async function fetchPost() {
-    try {
-        const response = await Axios.get('/post/${state.id}', {cancelToken: ourRequest.token})
-        dispatch({type: "fetchComplete", value: response.data})
-      } catch(e) {
-      console.log("There was a problem or the request was cancelled.")
-    }
+  function submitHandler(e) {
+    e.preventDefault()
+    dispatch({ type: "submitRequest" })
   }
-  fetchPost()
-  return () => {
-    ourRequest.cancel()
-  }
-}, [])
 
-if (state.isFetching) return <Page title={post.title}>
-  <LoadingDotsIcon />
-  </Page>
+  useEffect(() => {
+    const ourRequest = Axios.CancelToken.source()
+    async function fetchPost() {
+      try {
+        const response = await Axios.get(`/post/${state.id}`, { cancelToken: ourRequest.token })
+        dispatch({ type: "fetchComplete", value: response.data })
+      } catch (e) {
+        console.log("There was a problem or the request was cancelled.")
+      }
+    }
+    fetchPost()
+    return () => {
+      ourRequest.cancel()
+    }
+  }, [])
+
+  useEffect(() => {
+    if (state.sendCount) {
+      dispatch({ type: "saveRequestStarted" })
+      const ourRequest = Axios.CancelToken.source()
+      async function fetchPost() {
+        try {
+          const response = await Axios.post(`/post/${state.id}/edit`, { title: state.title.value, body: state.body.value, token: appState.user.token }, { cancelToken: ourRequest.token })
+          dispatch({ type: "saveRequestFinished" })
+          appDispatch({ type: "flashMessage", value: "Post was updated." })
+        } catch (e) {
+          console.log("There was a problem or the request was cancelled.")
+        }
+      }
+      fetchPost()
+      return () => {
+        ourRequest.cancel()
+      }
+    }
+  }, [state.sendCount])
+
+  if (state.isFetching)
+    return (
+      <Page title="...">
+        <LoadingDotsIcon />
+      </Page>
+    )
 
   return (
     <Page title="Edit Post">
-    <form >
+      <form onSubmit={submitHandler}>
         <div className="form-group">
           <label htmlFor="post-title" className="text-muted mb-1">
             <small>Title</small>
           </label>
-          <input value={state.title.value }autoFocus name="title" id="post-title" class="form-control form-control-lg form-control-title" type="text" placeholder="" autoComplete="off" />
+          <input onChange={e => dispatch({ type: "titleChange", value: e.target.value })} value={state.title.value} autoFocus name="title" id="post-title" className="form-control form-control-lg form-control-title" type="text" placeholder="" autoComplete="off" />
         </div>
 
-        <div class="form-group">
-          <label htmlFor="post-body" class="text-muted mb-1 d-block">
+        <div className="form-group">
+          <label htmlFor="post-body" className="text-muted mb-1 d-block">
             <small>Body Content</small>
           </label>
-          <textarea name="body" id="post-body" class="body-content tall-textarea form-control" type="text" value={state.body.value}/>
+          <textarea onChange={e => dispatch({ type: "bodyChange", value: e.target.value })} name="body" id="post-body" className="body-content tall-textarea form-control" type="text" value={state.body.value} />
         </div>
 
-        <button class="btn btn-primary">Save Updates</button>
+        <button className="btn btn-primary" disabled={state.isSaving}>
+          Save Updates
+        </button>
       </form>
-      </Page>
+    </Page>
   )
 }
 
