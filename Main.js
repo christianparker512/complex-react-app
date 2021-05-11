a03cf2384c221f540199d2df99348589e3b50979
Christian Parker <71667365+christianparker512@users.noreply.github.com>
2021-05-11 15:09:44 -0500

    Update Main.js

--- Main.js
+++ Main.js
@@ -1,78 +1,87 @@
-import React, {useState, useContext, useEffect} from "react"
+import React, { useState, useReducer, useEffect } from "react"
 import ReactDOM from "react-dom"
-import {useImmerReducer} from 'use-immer'
-import {BrowserRouter, Switch, Route} from "react-router-dom"
-import Axios from 'axios'
-Axios.defaults.baseURL = 'http://localhost:8080'
+import { useImmerReducer } from "use-immer"
+import { BrowserRouter, Switch, Route } from "react-router-dom"
+import Axios from "axios"
+Axios.defaults.baseURL = "http://localhost:8080"
 
-import StateContext from './StateContext'
-import DispatchContext from './DispatchContext'
+import StateContext from "./StateContext"
+import DispatchContext from "./DispatchContext"
 
-//my components
+// My Components
 import Header from "./components/Header"
-import Home from "./components/Home"
 import HomeGuest from "./components/HomeGuest"
+import Home from "./components/Home"
 import Footer from "./components/Footer"
 import About from "./components/About"
 import Terms from "./components/Terms"
 import CreatePost from "./components/CreatePost"
 import ViewSinglePost from "./components/ViewSinglePost"
-import FlashMessages from './components/FlashMessages'
-import Profile from './compenents/Profile'
-import EditPost from './componets/EditPost'
-function Main () {
+import FlashMessages from "./components/FlashMessages"
+import Profile from "./components/Profile"
+import EditPost from "./components/EditPost"
+import NotFound from "./components/NotFound"
+import Search from "./components/Search"
+
+function Main() {
   const initialState = {
-    loggedIn: Boolean(localStorage.getItem("complexappToken")), 
+    loggedIn: Boolean(localStorage.getItem("complexappToken")),
     flashMessages: [],
     user: {
-      token: localStorage.getItem("complexToken"),
-      username: localStorage.getItem("complexUsername"),
-      avatar: localStorage.getItem("complexAvatar"),
-    }
+      token: localStorage.getItem("complexappToken"),
+      username: localStorage.getItem("complexappUsername"),
+      avatar: localStorage.getItem("complexappAvatar")
+    },
+    isSearchOpen: false
   }
-  function ourReducer (draft,action){
-    switch (action.type){
+
+  function ourReducer(draft, action) {
+    switch (action.type) {
       case "login":
-        draft.loggedIn=true
-        draft.user = action.data 
-        return 
+        draft.loggedIn = true
+        draft.user = action.data
+        return
       case "logout":
-        draft.loggedIn=false
+        draft.loggedIn = false
         return
       case "flashMessage":
         draft.flashMessages.push(action.value)
         return
+      case "openSearch":
+        draft.isSearchOpen = true
+        return
+      case "closeSearch":
+        draft.isSearchOpen = false
+        return
     }
-    
   }
 
-  const [state, dispatch]= useImmerReducer(ourReducer, intialState)
- 
-  useEffect(() => {
-    if(state.loggedIn){
-      localStorage.setItem("complexappToken",state.user.token)
-      localStorage.setItem("complexappUsername",rstate.user.username)
-      localStorage.setItem("complexappAvatar",state.user.avatar)
-      props.setLoggedIn(true)
-    } else {
-      localStorage.removeItem("complexappToken",state.user.token)
-      localStorage.removeItem("complexappUsername",rstate.user.username)
-      localStorage.removeItem("complexappAvatar",state.user.avatar)
-    }
-  } ,[state.loggedIn])
+  const [state, dispatch] = useImmerReducer(ourReducer, initialState)
 
-return (
-   <StateContext.Provider value={state}>
-     <DispatchContext.Provider value ={dispatch}>
-    <BrowserRouter>
-      <FlashMessages messages={state.flashMessages}/>
-      <Header />
-        <Switch>
-          <Route path="/profile/:username">
-            <Profile />
-          </Route>
-          <Route path="/" exact>
-            {state.loggedIn ? <Home /> : <HomeGuest />}
+  useEffect(() => {
+    if (state.loggedIn) {
+      localStorage.setItem("complexappToken", state.user.token)
+      localStorage.setItem("complexappUsername", state.user.username)
+      localStorage.setItem("complexappAvatar", state.user.avatar)
+    } else {
+      localStorage.removeItem("complexappToken")
+      localStorage.removeItem("complexappUsername")
+      localStorage.removeItem("complexappAvatar")
+    }
+  }, [state.loggedIn])
+
+  return (
+    <StateContext.Provider value={state}>
+      <DispatchContext.Provider value={dispatch}>
+        <BrowserRouter>
+          <FlashMessages messages={state.flashMessages} />
+          <Header />
+          <Switch>
+            <Route path="/profile/:username">
+              <Profile />
+            </Route>
+            <Route path="/" exact>
+              {state.loggedIn ? <Home /> : <HomeGuest />}
             </Route>
             <Route path="/post/:id" exact>
               <ViewSinglePost />
@@ -80,21 +89,24 @@ return (
             <Route path="/post/:id/edit" exact>
               <EditPost />
             </Route>
-          <Route path="/create-post">
-            <CreatePost addFlashMessage={addFlashMessage}/>
-          </Route>
-          <Route path="/about-us">
-            <About />
-          </Route>
-          <Route path="/terms">
-            <Terms />
-          </Route>
+            <Route path="/create-post">
+              <CreatePost />
+            </Route>
+            <Route path="/about-us">
+              <About />
+            </Route>
+            <Route path="/terms">
+              <Terms />
+            </Route>
+            <Route>
+              <NotFound />
+            </Route>
           </Switch>
+          {state.isSearchOpen ? <Search /> : ""}
           <Footer />
-
-      </BrowserRouter>
+        </BrowserRouter>
       </DispatchContext.Provider>
-      </StateContext.Provider>
+    </StateContext.Provider>
   )
 }
 
